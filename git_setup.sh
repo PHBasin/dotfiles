43aca4279b6723ae5efdfc98fe70b4c0ea073869
PHBasin <pierre-henri.basin@orange.fr>
2020-10-30 14:44:08 +0100

    rm ask for name / email

--- git_setup.sh
+++ git_setup.sh
@@ -1,12 +1,6 @@
-echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
-read full_name
-
-echo "Type in your email address (the one used for your GitHub account): "
-read email
-
-export GITHUB_USERNAME=$full_name
-git config --global user.email $email
-git config --global user.name $full_name
+export GITHUB_USERNAME=PHBasin
+git config --global user.email "pierre-henri.basin@orange.fr"
+git config --global user.name "PHBasin"
 git config --global core.editor "vim"
 
 echo "Add the following key to your GitHub account (https://github.com/settings/ssh/new): "
@@ -14,9 +8,5 @@ mkdir -p ~/.ssh && ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519 -C "basi
 cat ~/.ssh/id_ed25519.pub
 
 echo "Check that this step is completed, in the terminal run this: ssh -T git@github.com"
-
-git add .
-git commit --message "My identity in the gitconfig"
-git push origin master
 
 echo "👌 Awesome, all set."

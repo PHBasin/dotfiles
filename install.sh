78db7b8ec167d0ffee818c7fd166834d3fa0d2fe
PHBasin <pierre-henri.basin@orange.fr>
2021-03-12 13:27:52 +0100

    autoindent commented

--- install.sh
+++ install.sh
@@ -1,26 +1,25 @@
 backup() {
   target=$1
   if [ -e "$target" ]; then           # Does the config file already exist?
-    if [ ! -L "$target" ]; then       # as a pure file?
-      mv "$target" "$target.backup"   # Then backup it
-      echo "-----> Moved your old $target config file to $target.backup"
-    fi
-  fi
+		if [ ! -L "$target" ]; then       # as a pure file?
+			mv "$target" "$target.backup"   # Then backup it
+			echo "-----> Moved your old $target config file to $target.backup"
+		fi
+	fi
 }
 
 #!/bin/zsh
 for name in *; do
-  if [ ! -a "$name" ]; then
-    target="$HOME/.$name"
-    if [[ ! "$name" =~ '\.sh$' ]]; then
-      backup $target
-
-      if [ ! -e "$target" ]; then
-        echo "-----> Symlinking your new $target"
-        ln -s "$PWD/$name" "$target"
-      fi
-    fi
-  fi
+	if [ ! -a "$name" ]; then
+		target="$HOME/.$name"
+		if [[ ! "$name" =~ '\.sh$' ]]; then
+			backup $target
+			if [ ! -e "$target" ]; then
+				echo "-----> Symlinking your new $target"
+				ln -s "$PWD/$name" "$target"
+			fi
+		fi
+	fi
 done
 
 REGULAR="\\033[0;39m"
@@ -32,8 +31,8 @@ CURRENT_DIR=`pwd`
 ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
 mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
 if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+	echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 fi
 cd "$CURRENT_DIR"
 

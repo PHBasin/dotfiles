f3691f5c818a09b823e9ea7c27c50a41ed53ee9b
PHBasin <pierre-henri.basin@orange.fr>
2021-07-06 17:22:27 +0200

    install.sh update function + zsh plugin

--- git_setup.sh
+++ git_setup.sh
@@ -3,10 +3,6 @@ git config --global user.email "pierre-henri.basin@orange.fr"
 git config --global user.name "PHBasin"
 git config --global core.editor "vim"
 
-echo "Add the following key to your GitHub account (https://github.com/settings/ssh/new): "
-mkdir -p ~/.ssh && ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519 -C "pierre-henri.basin@orange.fr"
-cat ~/.ssh/id_ed25519.pub
-
 echo "Check that this step is completed, in the terminal run this: ssh -T git@github.com"
 
 echo "👌 Awesome, all set."

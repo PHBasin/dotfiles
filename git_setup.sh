9f78ca6f6c5ea0b8fb69cadbaca2a8d0068ef023
PHBasin <pierre-henri.basin@orange.fr>
2021-07-15 20:51:05 +0200

    symlink function & ssh key for github removed

--- git_setup.sh
+++ git_setup.sh
@@ -4,8 +4,6 @@ git config --global user.name "PHBasin"
 git config --global core.editor "vim"
 
 echo "Add the following key to your GitHub account (https://github.com/settings/ssh/new): "
-mkdir -p ~/.ssh && ssh-keygen -t ed25519 -o -a 100 -f ~/.ssh/id_ed25519 -C "pierre-henri.basin@orange.fr"
-cat ~/.ssh/id_ed25519.pub
 
 echo "Check that this step is completed, in the terminal run this: ssh -T git@github.com"
 
